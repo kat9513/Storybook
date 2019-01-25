3056d02e5928cd9c08e21417a2c87d2589e398bf
Monroe Howard II <monroehowardii@Monroes-MacBook-Air.local>
2019-01-24 21:35:21 -0600

    updated html routes file

--- Storybookfiles/routes/html-routes.js
+++ Storybookfiles/routes/html-routes.js
@@ -13,21 +13,21 @@ module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
+  // index route loads home.html (add.html is the homepage)
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/view.html"));
+    res.sendFile(path.join(__dirname, "../views/layouts/add.html"));
   });
 
   // add route loads the add.html page,
-  // where users can enter new characters to the db
-  app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/add.html"));
+  // where users can enter new story to the db (all.html is the file where stories are entered as well as stored to the db)
+  app.get("/all", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/layouts/all.html"));
   });
 
   // all route loads the all.html page,
-  // where all characters in the db are displayed
-  app.get("/all", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/all.html"));
-  });
+  // where all stories in the db are displayed
+  //app.get("/all", function(req, res) {
+    //res.sendFile(path.join(__dirname, "../views/layouts/all.html"));
+  //});
 
 };
